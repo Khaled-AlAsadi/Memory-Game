173cab13b77e92309bbe24cf529216e0b2e9a92d
Khaled AL Asadi <khelo.asadi@gmail.com>
2023-12-14 20:43:44 +0100

    Remove aria-labels and add function.js for the functions

---
+++ assets/js/function.js
@@ -0,0 +1,11 @@
+ function viewRules(){
+       window.location.href = "rules.html";
+}
+function startGame(){
+    window.location.href = "game.html";
+}
+function returnToMenu() {
+    window.location.href = "index.html";
+}
+
+ 
