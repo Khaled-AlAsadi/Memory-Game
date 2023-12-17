67b04a991074d8760f8ba662141d1822c8f61ddc
Khaled AL Asadi <khelo.asadi@gmail.com>
2023-12-17 18:34:13 +0100

    Update testing file

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,11 +1,9 @@
- function viewRules(){
-       window.location.href = "rules.html";
+function viewRules() {
+  window.location.href = "rules.html";
 }
-function startGame(){
-    window.location.href = "game.html";
+function startGame() {
+  window.location.href = "game.html";
 }
 function returnToMenu() {
-    window.location.href = "index.html";
+  window.location.href = "index.html";
 }
-
- 
