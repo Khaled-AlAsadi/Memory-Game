1e89f29483cd81e7ed17b756e63fc441cc460caa
Khaled AL Asadi <khelo.asadi@gmail.com>
2023-12-19 20:57:55 +0100

    Update funtion.js

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,10 +1,20 @@
 /* jshint esversion: 8 */
+
+/**
+ * Funtion to naviagte to rules page
+ */
 function viewRules() {
   window.location.href = "rules.html";
 }
+/**
+ * Funtion to naviagte to game page
+ */
 function startGame() {
   window.location.href = "game.html";
 }
+/**
+ * Funtion to naviagte to menu page
+ */
 function returnToMenu() {
   window.location.href = "index.html";
 }
